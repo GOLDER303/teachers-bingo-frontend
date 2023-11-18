cb8299499eee42352c52ba82a9ecc0939725c7bd
GOLDER303 <szymonstepien@interia.pl>
2023-11-18 16:46:22 +0100

    Create main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,45 @@
+import { useRef } from "react"
+
 const MainPage: React.FC = () => {
-    return <h1>Main Page</h1>
+    const playerNameInputRef = useRef<HTMLInputElement>(null)
+
+    const handlePlayerNameSubmit = () => {
+        // TODO:
+    }
+
+    return (
+        <div className="flex flex-col h-screen pb-20 items-center justify-center bg-blue-500 text-white">
+            <h1 className="mb-20 text-5xl font-bold">Teacher's Bingo</h1>
+            <form
+                className="flex flex-col h-1/4 items-center justify-between"
+                onSubmit={(event) => {
+                    event.preventDefault()
+                    handlePlayerNameSubmit()
+                }}
+            >
+                <label
+                    className="text-3xl"
+                    htmlFor="playerName"
+                >
+                    Enter Your Name:
+                </label>
+                <input
+                    className="p-1 text-2xl text-neutral-700 text-center rounded-lg"
+                    type="text"
+                    name="playerName"
+                    id="playerName"
+                    placeholder="Your Name"
+                    ref={playerNameInputRef}
+                />
+                <button
+                    className="px-10 py-3 text-2xl border-2 border-white rounded-xl"
+                    type="submit"
+                >
+                    Play
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default MainPage
