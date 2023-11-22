7e3af6c27a40ece1dd1b7a0cd2f2835b91fa8190
GOLDER303 <szymonstepien@interia.pl>
2023-11-22 19:45:35 +0100

    Display an error if the player info is missing on the bingo game page

--- src/pages/BingoGamePage.tsx
+++ src/pages/BingoGamePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import { PlayerInfoDTO } from "../dtos/PlayerInfoDTO"
 import { getPlayerInfo } from "../services/PlayerService"
 
@@ -32,28 +32,43 @@ const BingoGamePage: React.FC = () => {
             <h1 className="mb-10 text-3xl font-bold">Bingo Name</h1>
 
             {/* bingo grid */}
-            <div className="grid max-w-3xl grid-cols-3 grid-rows-3 gap-1 md:gap-2">
-                {playerInfo?.currentPhrases.map((row, rowIndex) => {
-                    return (
-                        <>
-                            {row.map((phrase, colIndex) => {
-                                return (
-                                    <div
-                                        className="flex w-full h-full p-3 aspect-square items-center justify-center bg-blue-400 rounded-xl cursor-pointer hover:bg-blue-300 md:p-5"
-                                        onClick={() => {
-                                            handlePhraseClick(rowIndex, colIndex)
-                                        }}
-                                    >
-                                        <p className="text-base leading-none text-center sm:text-xl lg:leading-snug">
-                                            {phrase}
-                                        </p>
-                                    </div>
-                                )
-                            })}
-                        </>
-                    )
-                })}
-            </div>
+            {playerInfo ? (
+                <div className="grid max-w-3xl grid-cols-3 grid-rows-3 gap-1 md:gap-2">
+                    {playerInfo.currentPhrases.map((row, rowIndex) => {
+                        return (
+                            <>
+                                {row.map((phrase, colIndex) => {
+                                    return (
+                                        <div
+                                            className="flex w-full h-full p-3 aspect-square items-center justify-center bg-blue-400 rounded-xl cursor-pointer hover:bg-blue-300 md:p-5"
+                                            onClick={() => {
+                                                handlePhraseClick(rowIndex, colIndex)
+                                            }}
+                                        >
+                                            <p className="text-base leading-none text-center sm:text-xl lg:leading-snug">
+                                                {phrase}
+                                            </p>
+                                        </div>
+                                    )
+                                })}
+                            </>
+                        )
+                    })}
+                </div>
+            ) : (
+                <div className="flex flex-col mt-32 gap-9 items-center justify-between">
+                    <h2 className="text-2xl text-center">Error while fetching player info. Try again later</h2>
+
+                    <button className="px-8 py-3 border-2 border-white rounded-lg cursor-pointer shadow-lg lg:px-12 lg:py-4">
+                        <Link
+                            className="relative text-md text-center lg:text-lg"
+                            to={"/"}
+                        >
+                            Go to Home Page
+                        </Link>
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
